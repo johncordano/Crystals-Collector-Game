f97e58fb35ccd35b48ee7c1d19fb052628ea3599
John Cordano <jecordano@yahoo.com>
2018-01-24 22:37:42 -0800

    Copied funtionality from one crystal to other crystals, set up win/loss functionality, set up mouse hover hand for images, added comments.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,10 +11,10 @@ var lossesStart = 0;
 var losses = 0;
 // Creates variable for the user wins at the start of a game session.
 var winsStart = 0;
-// Creates a variable for the number of time the user wins a game session.
+// Creates a variable for the number of times the user wins a game session.
 var wins = 0;
 
-// Determines that the web page is ready for manipulation
+// Determines that the web page is ready for manipulation.
 $(document).ready(function() {
 
 // Creates the random computer number that the user must match by clicking the crystal icons.
@@ -30,7 +30,6 @@ $(document).ready(function() {
  console.log(secondCrystalNumber)
  console.log(thirdCrystalNumber)
  console.log(fourthCrystalNumber)
- console.log(userTotalScoreStart)
  
 
 
@@ -41,25 +40,71 @@ $("#Wins").html(winsStart);
 $("#score").html(userTotalScoreStart);
 
 
-
+// Updates the user total score when the user clicks the first crystal image.
 $("#firstButton").on("click", function () {
-	userTotalScore = (userTotalScore) + (firstCrystalNumber);
-	$("#score").html(userTotalScore);
+	if (userTotalScore < computerGenNumber) {
+		userTotalScore = (userTotalScore) + (firstCrystalNumber);
+		$("#score").html(userTotalScore);
+	}
+});
+
+// Updates the user total score when the user clicks the second crystal image.	
+$("#secondButton").on("click", function () {
+ 	if (userTotalScore < computerGenNumber) {
+ 		userTotalScore = (userTotalScore) + (secondCrystalNumber);
+ 		$("#score").html(userTotalScore);
+ 	}
+}); 
+
+// Updates the user total score when the user clicks the third crystal image.
+$("#thirdButton").on("click", function () {
+ 	if (userTotalScore < computerGenNumber) {
+ 		userTotalScore = (userTotalScore) + (thirdCrystalNumber);
+ 		$("#score").html(userTotalScore);
+ 	}
+});
+
+// Updates the user total score when the user clicks the fourth crystal image.
+$("#fourthButton").on("click", function () {
+ 	if (userTotalScore < computerGenNumber) {
+ 		userTotalScore = (userTotalScore) + (fourthCrystalNumber);
+ 		$("#score").html(userTotalScore);
+ 	}
+});	
+
+
+// Updates the win and loss data when the user clicks any crystal that wins or losses a game session.
+$(".buttonCrystals").on("click", function () {			
 	if (userTotalScore === computerGenNumber) {
+		wins = wins + 1;
+		$("#message").html("You win!");
+		$("#Wins").html(wins);
 		userTotalScore = 0;
 		$("#score").html(userTotalScore);
-		Wins = Wins + 1;
-		// Regenerate crystal numbers and computer number.
+		// var computerGenNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+		// $("#Computer-Number").html(computerGenNumber);
+		// var firstCrystalNumber = Math.floor(Math.random() * 12) + 1;
+ 		// var secondCrystalNumber = Math.floor(Math.random() * 12) + 1;
+ 		// var thirdCrystalNumber = Math.floor(Math.random() * 12) + 1;
+ 		// var fourthCrystalNumber = Math.floor(Math.random() * 12) + 1;
 	}
-
 	if (userTotalScore > computerGenNumber) {
+		losses = losses + 1;
+		$("#message").html("You lose!");
+		$("#Losses").html(losses);
 		userTotalScore = 0;
-		Losses = Losses + 1;
-		// Regenerate crystal numbers and computer number.
-	} 
-	
-	console.log(userTotalScore)
-})
+		$("#score").html(userTotalScore);
+		// var computerGenNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+		// $("#Computer-Number").html(computerGenNumber);
+		// var firstCrystalNumber = Math.floor(Math.random() * 12) + 1;
+ 		// var secondCrystalNumber = Math.floor(Math.random() * 12) + 1;
+ 		// var thirdCrystalNumber = Math.floor(Math.random() * 12) + 1;
+ 		// var fourthCrystalNumber = Math.floor(Math.random() * 12) + 1;
+		}
+
+});
+
+
 
 
 
